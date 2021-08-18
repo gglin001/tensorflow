776e69a08f9130c5f9fbd92e5ca333e71d224e89
Allen Guo <guosonglin001@qq.com>
2021-08-18 20:36:58 +0800

    add compdb in mac

--- demos/demo.py
+++ demos/demo.py
@@ -3,10 +3,10 @@ import sys
 
 print(os.getpid())
 
-tensorflow_path = (
-    '/data/songlin/repos/tensorflow/bazel-bin/tensorflow/tools/pip_package/build_pip_package.runfiles/org_tensorflow'
-)
-sys.path.insert(0, tensorflow_path)
+# tensorflow_path = (
+#     '/Users/allen/gglin001/tensorflow/bazel-bin/tensorflow/tools/pip_package/build_pip_package.runfiles/org_tensorflow'
+# )
+# sys.path.insert(0, tensorflow_path)
 import tensorflow as tf
 
 print(tf.__file__)
